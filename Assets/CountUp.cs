1ebfcb48b64a0fce1a67050386e79db412f4eb0c
WSTK-Felix <yajie.sun@weistek.net>
2019-11-26 11:22:15 +0800

    测试build

---
+++ Assets/CountUp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountUp : MonoBehaviour
+{
+    public Text label;
+
+    private int count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(countUp());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        label.text = count.ToString();
+    }
+
+    IEnumerator countUp()
+    {
+        while (true)
+        {
+            count++;
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+}
